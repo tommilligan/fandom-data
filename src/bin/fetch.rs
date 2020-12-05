b00c3a4d74760058dd3fc9b32edfac1c702c344c
Tom Milligan <tom@reinfer.io>
2020-12-05 15:25:20 +0000

    add thread sleeping to avoid rate limiting

--- src/bin/fetch.rs
+++ src/bin/fetch.rs
@@ -1,6 +1,7 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use ao3_fandom_vis::{request::page, scrape::search_page_to_works};
 use reqwest::{blocking::Client, Url};
+use std::{thread::sleep, time::Duration};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -11,6 +12,10 @@ struct Opt {
 
     #[structopt(long = "count", default_value = "1")]
     count: u32,
+
+    /// Interval between request, to avoid rate limiting.
+    #[structopt(long = "interval", default_value = "10")]
+    interval: u64,
 }
 
 fn main() -> Result<()> {
@@ -18,6 +23,7 @@ fn main() -> Result<()> {
 
     let opt = Opt::from_args();
 
+    let interval = Duration::from_secs(opt.interval);
     let page_start = opt.start;
     let page_count = opt.count;
     let page_end = page_start + page_count;
@@ -25,8 +31,10 @@ fn main() -> Result<()> {
 
     for page_number in page_start..page_end {
         log::info!("Processing page {}", page_number);
-        let works =
-            search_page_to_works(&client.get(Url::parse(&page(page_number))?).send()?.text()?)?;
+
+        let url = Url::parse(&page(page_number))?;
+        let html = &client.get(url).send()?.text()?;
+        let works = search_page_to_works(html)?;
         if works.is_empty() {
             log::info!("Received no works on page {}, breaking", page_number);
             break;
@@ -34,6 +42,8 @@ fn main() -> Result<()> {
         for work in works.iter() {
             println!("{}", serde_json::to_string(work)?);
         }
+
+        sleep(interval)
     }
     Ok(())
 }
