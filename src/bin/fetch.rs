65e829b832e3b3b898699849e51d4bc310847067
Tom Milligan <tom@reinfer.io>
2020-12-05 14:28:30 +0000

    add structopt

--- src/bin/fetch.rs
+++ src/bin/fetch.rs
@@ -1,12 +1,25 @@
 use anyhow::{Context, Result};
 use ao3_fandom_vis::{request::page, scrape::search_page_to_works};
 use reqwest::{blocking::Client, Url};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "fetch", about = "Fetch ao3 data")]
+struct Opt {
+    #[structopt(long = "start", default_value = "1")]
+    start: u32,
+
+    #[structopt(long = "count", default_value = "1")]
+    count: u32,
+}
 
 fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let page_start = 1;
-    let page_count = 2;
+    let opt = Opt::from_args();
+
+    let page_start = opt.start;
+    let page_count = opt.count;
     let page_end = page_start + page_count;
     let client = Client::new();
 
