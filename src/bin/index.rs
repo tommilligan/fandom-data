bb14c5c8617f3d991a10421c18933f51ef6907f4
Tom Milligan <tom@reinfer.io>
2020-12-22 20:54:25 +0000

    working visualisation of romantic/platonic

---
+++ src/bin/index.rs
@@ -0,0 +1,121 @@
+use anyhow::{Context, Result};
+use ao3_fandom_vis::scrape::Work;
+use elasticsearch::{
+    http::transport::Transport,
+    indices::{Indices, IndicesPutMappingParts},
+    BulkOperation, BulkOperations, BulkParts, Elasticsearch,
+};
+use itertools::Itertools;
+use once_cell::sync::Lazy;
+use serde_json::{json, Value};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+use structopt::StructOpt;
+
+const WORKS_INDEX: &str = "works";
+
+static MAPPING_WORKS: Lazy<Value> = Lazy::new(|| {
+    json!({
+      "properties": {
+        "id": {
+          "type": "keyword"
+        },
+        "title": {
+          "type": "text"
+        },
+        "author": {
+          "type": "keyword"
+        },
+        "relationships": {
+          "type": "keyword"
+        },
+        "characters": {
+          "type": "keyword"
+        },
+        "freeforms": {
+          "type": "keyword"
+        },
+        "date": {
+          "type": "date"
+        },
+        "language": {
+          "type": "keyword"
+        },
+        "words": {
+          "type": "long"
+        },
+        "kudos": {
+          "type": "long"
+        },
+        "hits": {
+          "type": "long"
+        },
+      }
+    })
+});
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "fetch", about = "Fetch ao3 data")]
+struct Opt {
+    /// Works data to index
+    #[structopt(long = "input")]
+    input: PathBuf,
+
+    /// Endpoint of elasticsearch cluster
+    #[structopt(long = "elasticsearch")]
+    elasticsearch: String,
+
+    /// Document chunk size to upload in one request
+    #[structopt(long = "chunk-size", default_value = "1024")]
+    chunk_size: usize,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    let opt = Opt::from_args();
+
+    let transport = Transport::single_node(&opt.elasticsearch)?;
+    let client = Elasticsearch::new(transport);
+    let indices = Indices::new(client.transport());
+
+    indices
+        .put_mapping(IndicesPutMappingParts::Index(&[WORKS_INDEX]))
+        .body(&*MAPPING_WORKS);
+
+    let file = BufReader::new(File::open(opt.input).context("input file")?);
+    for (chunk_index, lines) in file.lines().chunks(opt.chunk_size).into_iter().enumerate() {
+        log::info!(
+            "Processing chunk {} ({} documents)",
+            chunk_index,
+            (chunk_index + 1) * opt.chunk_size
+        );
+        let mut ops = BulkOperations::new();
+        for line in lines.into_iter() {
+            let work: Work =
+                serde_json::from_str(&line.context("input line")?).context("line json")?;
+            let id = work.id.clone();
+            ops.push(BulkOperation::index(work).id(id))?;
+        }
+
+        let response = client
+            .bulk(BulkParts::Index("works"))
+            .body(vec![ops])
+            .send()
+            .await?;
+
+        // get the HTTP response status code
+        let status_code = response.status_code();
+
+        // read the response body. Consumes search_response
+        let response_body = response.json::<Value>().await?;
+
+        // read fields from the response body
+        let took = response_body["took"].as_i64().unwrap();
+    }
+
+    Ok(())
+}
---
+++ src/bin/ships.rs
@@ -0,0 +1,211 @@
+use anyhow::{anyhow, Context, Error, Result};
+use chord::{Chord, Plot};
+use elasticsearch::{http::transport::Transport, Elasticsearch, SearchParts};
+use itertools::Itertools;
+use serde_json::{json, Value};
+use std::{collections::HashMap, str::FromStr};
+use structopt::StructOpt;
+
+const WORKS_INDEX: &str = "works";
+const AGGREGATION_KEY: &str = "aggregation_key";
+const FIELD_RELATIONSHIPS_KEYWORD: &str = "relationships.keyword";
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "fetch", about = "Fetch ao3 data")]
+struct Opt {
+    /// Endpoint of elasticsearch cluster
+    #[structopt(long = "elasticsearch")]
+    elasticsearch: String,
+
+    /// Top n ships to display.
+    #[structopt(long = "limit", default_value = "20")]
+    limit: usize,
+
+    /// Relationship type to display.
+    #[structopt(long = "ship-type", default_value = "romantic")]
+    ship_type: Ship,
+}
+
+async fn relationship_frequencies(
+    client: &Elasticsearch,
+    limit: usize,
+) -> Result<Vec<(String, u64)>> {
+    let response = client
+        .search(SearchParts::Index(&[WORKS_INDEX]))
+        .body(json!({
+          "aggs": {
+              AGGREGATION_KEY: {
+                "terms": {
+                  "field": FIELD_RELATIONSHIPS_KEYWORD,
+                  "order": {
+                    "_count": "desc"
+                  },
+                  "size": limit
+                }
+              }
+            },
+          "size": 0,
+          "query": {
+              "match_all": {}
+          }
+        }))
+        .allow_no_indices(true)
+        .send()
+        .await?;
+
+    let response_body = response.json::<Value>().await?;
+    let buckets = response_body
+        .get("aggregations")
+        .context("Response aggregations key")?
+        .get(AGGREGATION_KEY)
+        .context("Response aggregation key")?
+        .get("buckets")
+        .context("Response buckets key")?
+        .as_array()
+        .context("Response buckets array")?;
+    Ok(buckets
+        .into_iter()
+        .map(|bucket| {
+            Ok((
+                bucket
+                    .get("key")
+                    .context("bucket key")?
+                    .as_str()
+                    .context("bucket key string")?
+                    .to_owned(),
+                bucket
+                    .get("doc_count")
+                    .context("bucket doc count")?
+                    .as_u64()
+                    .context("bucket doc count integer")?,
+            ))
+        })
+        .collect::<Result<_>>()?)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    let opt = Opt::from_args();
+
+    let transport = Transport::single_node(&opt.elasticsearch)?;
+    let client = Elasticsearch::new(transport);
+
+    let mut freqs: Vec<_> = relationship_frequencies(&client, opt.limit)
+        .await?
+        .into_iter()
+        .filter_map(|(ship, count)| ship_to_characters(&ship).map(|characters| (characters, count)))
+        .filter(|(ship, _count)| ship.1 == opt.ship_type)
+        .collect();
+    let original_freq_length = freqs.len();
+    freqs.sort_by_key(|(ship, count)| (ship.0.clone(), u64::MAX - count));
+    freqs.dedup_by_key(|(ship, _count)| ship.0.clone());
+    let dedup_freq_length = freqs.len();
+    let removed_length = original_freq_length - dedup_freq_length;
+    if removed_length > 0 {
+        log::warn!("Removed {} duplicate ship tags", removed_length);
+    }
+
+    // Count up mentions of each character
+    let mut characters: HashMap<&str, u64> = HashMap::default();
+    for (ship, count) in freqs.iter() {
+        *characters.entry(&ship.0 .0).or_default() += count;
+        *characters.entry(&ship.0 .1).or_default() += count
+    }
+
+    let mut character_list = characters
+        .keys()
+        .map(|character| (*character).to_owned())
+        .collect::<Vec<String>>();
+    character_list.sort_unstable();
+    let character_index: HashMap<&str, usize> = character_list
+        .iter()
+        .enumerate()
+        .map(|(index, character)| (character.as_ref(), index))
+        .collect();
+
+    let mut matrix: Vec<Vec<f64>> = vec![vec![0.; character_list.len()]; character_list.len()];
+    for (ship, count) in freqs.iter() {
+        // let character_one_freq: f64 = *count as f64
+        //     / *characters
+        //         .get(&character_one.as_ref())
+        //         .expect("character to have total frequency") as f64;
+        // let character_two_freq: f64 = *count as f64
+        //     / *characters
+        //         .get(&character_two.as_ref())
+        //         .expect("character to have total frequency") as f64;
+        let character_one_index = *character_index
+            .get(&ship.0 .0.as_ref())
+            .expect("character to have index");
+        let character_two_index = *character_index
+            .get(&ship.0 .1.as_ref())
+            .expect("character to have index");
+        matrix[character_one_index][character_two_index] = *count as f64;
+        matrix[character_two_index][character_one_index] = *count as f64;
+    }
+
+    Chord {
+        matrix,
+        names: character_list,
+        wrap_labels: false,
+        width: 1150.,
+        margin: 75.,
+        font_size_large: "14px".to_owned(),
+        ..Chord::default()
+    }
+    .to_html();
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq)]
+enum Ship {
+    Romantic,
+    Platonic,
+}
+
+impl FromStr for Ship {
+    type Err = Error;
+
+    fn from_str(string: &str) -> Result<Self> {
+        match string {
+            "romantic" => Ok(Self::Romantic),
+            "platonic" => Ok(Self::Platonic),
+            _ => Err(anyhow!("Invalid ship type: '{}'", string)),
+        }
+    }
+}
+
+/// Given a ship tag, returns a list of characters in the ship.
+///
+/// A bit of data munging here to remove duplicates.
+fn ship_to_characters(original_ship: &str) -> Option<((String, String), Ship)> {
+    let mut ship = original_ship;
+    // Trim off fandom if present
+    if let Some(fandom_start) = ship.find('(') {
+        ship = &ship[..fandom_start];
+    }
+
+    // Split on separators
+    let mut characters: Vec<String> = ship
+        .split(&['/', '&'][..])
+        .map(|s| s.trim().to_owned())
+        .collect();
+
+    // Coocurrance matrix doesn't work for poly ships?
+    if characters.len() != 2 {
+        log::warn!("Invalid characters length: '{}'", original_ship);
+        return None;
+    }
+    characters.sort_unstable();
+    let characters: (String, String) = characters.into_iter().collect_tuple().unwrap();
+
+    if ship.contains('/') {
+        Some((characters, Ship::Romantic))
+    } else if ship.contains('&') {
+        Some((characters, Ship::Platonic))
+    } else {
+        log::warn!("Invalid ship name: '{}'", original_ship);
+        None
+    }
+}
